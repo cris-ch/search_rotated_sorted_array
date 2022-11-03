4300a39228b903f4c9e1165ac9232a47c73da566
Cristian <cristian.christensen@gmail.com>
2022-11-03 19:14:40 +0100

    4 tests passing

--- src/search.js
+++ src/search.js
@@ -1,6 +1,6 @@
 const search = (nums, target) => {
-  if (target === nums[0]) {
-    return 0;
+  if (target === nums[0] || target === nums[1]) {
+    return nums.indexOf(target);
   } else {
     return -1;
   }
--- test/search.test.js
+++ test/search.test.js
@@ -8,4 +8,12 @@ describe("search", () => {
   it("should return the index of target if it is in nums.", () => {
     expect(search([1], 1)).toBe(0);
   });
+
+  it("should return -1 if index of target it is not in nums.", () => {
+    expect(search([1, 2], 0)).toBe(-1);
+  });
+
+  it("should return the index of target if it is in nums.", () => {
+    expect(search([1, 2], 2)).toBe(1);
+  });
 });
